f693ef439214fe4421573c9bf7ce5c73fa32a1ae
Naveed247365 <qureshinaveed21@hotmail.com>
2025-01-04 02:49:17 -0800

    1st

--- src/app/About/page.tsx
+++ src/app/About/page.tsx
@@ -7,7 +7,7 @@ const About = () => {
           <h2 className='text-4xl font-bold mb-4' data-aos="fade-up">About Us</h2>
           <p>Welcome to our EirBuds website! We are a passionate team dedicated to providing you with the best headphones for your music experience.
             Our mission is to curate a collection of headphones that combine exceptional sound quality with stylish designs to enhance your listening experience.
-            Whether you're a music enthusiast or a fan of high-quality audio, our EirBuds website has something for everyone.
+            Whether you are a music enthusiast or a fan of high-quality audio, our EirBuds website has something for everyone.
             Shop now and discover the perfect headphones for your music journey.
           </p>
         </div>
